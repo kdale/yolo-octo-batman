4df8e596d341db8083e36c86a72d7a5862a3ddbe
kdale <krisdale@gmail.com>
2013-11-03 16:13:28 +0100

    Implemented BasicHttpResponse (4.c) and started basic implementation in BasicHttpWorker (4.d) - basic response and display in browser. File system interaction (retrieval of file, etc) not implemented.
    1.a, b, c, part of d complete and first stage testing done.

--- src/de/uulm/mi/web/http/impl/BasicHttpResponse.java
+++ src/de/uulm/mi/web/http/impl/BasicHttpResponse.java
@@ -1,5 +1,6 @@
 package de.uulm.mi.web.http.impl;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import de.uulm.mi.web.http.HttpResponse;
@@ -9,34 +10,57 @@ import de.uulm.mi.web.http.HttpVersion;
 public class BasicHttpResponse implements HttpResponse
 {
 	// TODO: add setters/appropriate constructors
-	private HttpVersion httpVersion;
-
+	private HttpVersion httpVersion = HttpVersion.VERSION_1_1; 
+	private HttpStatusCode statusCode;
+	private Map<String, String> responseHeaders = new HashMap<String, String>();
+	private byte[] entity;
+	
+	public void addHeader(String key, String value)
+	{
+		responseHeaders.put(key, value);
+	}
+	
+	public void setStatusCode(HttpStatusCode code)
+	{
+		statusCode = code;
+	}
+	
+	public void setEntityString(String entityString) 
+	{
+		entity = entityString.getBytes();
+	}
+	
+	public void setEntity(byte[] entityBytes)
+	{
+		entity = entityBytes;
+	}
+	
 	@Override
 	public HttpVersion getHttpVersion()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return httpVersion;
 	}
 
 	@Override
 	public Map<String, String> getHeaders()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return responseHeaders;
 	}
 
 	@Override
 	public byte[] getEntity()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return entity;
 	}
 
 	@Override
 	public HttpStatusCode getStatusCode()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return statusCode;
 	}
 
 }
--- src/de/uulm/mi/web/server/impl/BasicHttpWorker.java
+++ src/de/uulm/mi/web/server/impl/BasicHttpWorker.java
@@ -4,15 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
 
+import de.uulm.mi.web.http.HttpMethod;
 import de.uulm.mi.web.http.HttpRequest;
 import de.uulm.mi.web.http.HttpResponse;
+import de.uulm.mi.web.http.HttpStatusCode;
 import de.uulm.mi.web.http.impl.BasicHttpRequest;
 import de.uulm.mi.web.http.impl.BasicHttpResponse;
 import de.uulm.mi.web.server.HttpWorker;
 
 public class BasicHttpWorker extends HttpWorker
 {
+	private String responseCodeHtml = "<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\"><TITLE>CODE PHRASE</TITLE></HEAD><BODY><H1>CODE PHRASE</H1></BODY></HTML>";
+	
 	public BasicHttpWorker(Socket socket, BasicHttpServer server)
 	{
 		super(socket, server);
@@ -30,9 +36,33 @@ public class BasicHttpWorker extends HttpWorker
 	protected HttpResponse handleRequest(HttpRequest request)
 	{
 		// TODO Auto-generated method stub
-		// we have the request info i.e method, resource, version - need to handle as appropriate
+		// Creates a response object based on the incoming request object, including reading in content from the file system, etc.
 		BasicHttpResponse basicHttpResponse = new BasicHttpResponse();
 		
+		// **** NOTE!!! **** //
+		// For testing purposes, the if/else is commented out - hard code a 501 response to verify request/server response.
+		// Uncomment if/else to handle GET
+		// **** ------- **** //
+		
+		// ** For starters, implement ONLY GET method, return error code for all other cases.
+		//if (request.getHttpMethod().toString().equals(HttpMethod.GET.toString())) 		// get request method
+		//{
+			// get request uri
+			// does the uri exist on file system? 
+			// if yes... 
+			// 		set response obj properties: Status code, reason phrase, header string, entity 
+			// 		read in file contents, put in response object
+			// 		EXAMPLE below in sendResponse
+			// if no...
+			// 		
+		//}
+		//else {
+			// base case, return "Not implemented" error
+			basicHttpResponse.setStatusCode(HttpStatusCode.NOT_IMPLEMENTED);
+			String codeHtmlResponse = responseCodeHtml.replaceAll("CODE", String.valueOf(basicHttpResponse.getStatusCode().getCode()));
+			codeHtmlResponse = codeHtmlResponse.replaceAll("PHRASE", basicHttpResponse.getStatusCode().getReasonPhrase());
+			basicHttpResponse.setEntityString(codeHtmlResponse);
+		//}
 		
 		return basicHttpResponse;
 	}
@@ -41,8 +71,41 @@ public class BasicHttpWorker extends HttpWorker
 	protected void sendResponse(HttpResponse response, OutputStream outputStream) throws IOException
 	{
 		// TODO Auto-generated method stub
-		String test = "hello world";
-		outputStream.write(test.getBytes());
+		// Send the already created Response obj to the client 
+		
+		// Build up our Response
+		StringBuilder responseString = new StringBuilder();
+		responseString.append(response.getHttpVersion().toString() + " ");
+		responseString.append(response.getStatusCode().getCode() + " ");
+		responseString.append(response.getStatusCode().getReasonPhrase());
+		responseString.append("\r\n");
+		responseString.append(getHeaderString(response));
+		responseString.append("\r\n"); 
+		
+		outputStream.write(responseString.toString().getBytes());
+		outputStream.write(response.getEntity());
+		outputStream.flush();
+	}
+	
+	/**
+	 * Helper method to build up the HTTP response header for output to the stream
+	 * @param response
+	 * @return String
+	 */
+	public String getHeaderString(HttpResponse response)
+	{
+		Iterator iterator = response.getHeaders().entrySet().iterator();
+		StringBuilder headerString = new StringBuilder();
+		
+		while (iterator.hasNext())
+		{
+			Map.Entry pairs = (Map.Entry)iterator.next();
+			headerString.append(pairs.getKey() + ": ");
+			headerString.append(pairs.getValue());
+			headerString.append("\r\n");
+		}
+		
+		return headerString.toString();
 	}
 
 	@Override
